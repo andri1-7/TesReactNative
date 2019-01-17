d57ab753916fb24e9c831c3a2e1a5bb31df654b9
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-01-17 23:48:16 +0700

    create counter page  - with redux usage example  - using redux actions and reducers

---
+++ src/Pages/Counter.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+import {
+  Button,
+  Icon
+} from 'native-base';
+
+/**
+ * Redux
+ */
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import AppActions from '../Actions';
+
+import MainStyle from '../Styles/MainStyle';
+
+class Counter extends Component {
+  render() {
+    return(
+      <View style={MainStyle.container}>
+        <View style={{flexDirection: 'row'}}>
+          <Button danger small onPress={() => this.props.counterStrike(false)}>
+            <Icon name="minus" type="FontAwesome"/>
+          </Button>
+          <Button success small onPress={() => this.props.counterStrike(true)}>
+            <Icon name="plus" type="FontAwesome"/>
+          </Button>
+        </View>
+        <Text>Counter { this.props.counter }</Text>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  counter: state.counter
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(AppActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
