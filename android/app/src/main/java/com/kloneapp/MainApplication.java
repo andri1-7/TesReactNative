7119c9258988c7e1af4dcb30b71d26d06757bfe9
Ajhoel Tragedy <panduwiranata43@gmail.com>
2019-01-27 22:06:26 +0700

    Install Plugin  - React native maps (react-native-maps)  - fix android gradle sdk & build tools version

--- android/app/src/main/java/com/kloneapp/MainApplication.java
+++ android/app/src/main/java/com/kloneapp/MainApplication.java
@@ -2,6 +2,7 @@ package com.kloneapp;
 
 import android.app.Application;
 
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new MapsPackage(),
           new RNGestureHandlerPackage(),
           new PickerPackage(),
           new VectorIconsPackage()
